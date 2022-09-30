3d4bf296e1ceff7ff6636679ffe3649265bddeec
lzanabria <lazanabria2@misena.edu.co>
2022-09-30 00:42:25 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let cant, nums, suma=0, resta=0, multi=1, divi=1;
-        cant=Number(prompt("Ingrese la cantidad de números: "));
-        for(let i=1; i<=cant; i++){
-            nums=Number(prompt("Ingrese el valor del número "+(i)+": "));
-            suma+=nums;
-            resta-=nums;
-            multi*=nums;
-            divi/=nums;
-        }
-        alert("\t Suma: "+suma+"\n\t Resta: "+resta+"\n\t Multiplicación: "+multi+"\n\t División: "+divi);
+        let prec, uds, subtot, val_iva, total;
+        const iva=16;
+        prec=Number(prompt("Ingrese el precio del producto: "));
+        uds=Number(prompt("Ingrese la cantidad de unidades: "));
+        subtot=prec*uds;
+        val_iva=(subtot*iva)/100;
+        total=subtot+val_iva;
+        alert("\tPrecio por unidad: $"+prec+"\n\tUnidades: "+uds+"\n\tSubtotal: $"+subtot+"\n\tIVA: "+iva+"%"+"\n\tValor del IVA: $"+val_iva+"\n\tPago total: $"+total);
     }, 500);
 });
