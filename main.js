27c39009416953516640829acabfc2648c3114f1
lzanabria <lazanabria2@misena.edu.co>
2022-09-29 23:05:41 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 addEventListener("DOMContentLoaded", (e) => {
     setTimeout(() => {
-        let a=0, b=1;
-        alert("1")
-        for(let i=1; i<=11; i++) {
-            a=b+4;
-            b=a-2;
-            alert(a);
-            alert(b);
+        let est, cal, cal_acum=0, prom, mayor=0, menor=10;
+        est=Number(prompt("Ingrese la cantidad de estudiantes: "));
+        for(let i=1; i<=est; i++){
+            cal=Number(prompt("Ingrese la calificación (1 - 10): "));
+            cal_acum+=cal;
+            if(cal>=mayor){
+                mayor=cal;
+            }
+            if(cal<=menor){
+                menor=cal;
+            }
         }
+        prom=cal_acum/est;
+        alert("▶ Estudiantes encuestados: "+est+"\n\t Promedio de calificaciones: "+prom+"\n\t Calificación más alta: "+mayor+"\n\t Calificación más baja: "+menor);
     }, 500);
 });
